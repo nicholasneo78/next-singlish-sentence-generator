3c04233fcaf489132915aac757ccb1cafd488afa
Nicholas Neo <nicholasneo78@gmail.com>
2020-12-02 11:39:03 +0800

    add lost files

--- FBScraper.py
+++ FBScraper.py
@@ -5,11 +5,11 @@
 import xlsxwriter
 from facebook_scraper import get_posts
 
-#Prompt for input
+# Prompt for input
 targetPage  = input("Target facebook page to scrape: ")
 noOfPages = int(input("Number of pages: "))
 
-#Set up workbook/worksheet
+# Set up workbook/worksheet
 workbook = xlsxwriter.Workbook('./scrapedData/scrapingData_'+str(targetPage)+'_FULL.xlsx') 
 worksheet = workbook.add_worksheet() 
 
@@ -17,12 +17,12 @@ worksheet = workbook.add_worksheet()
 # Rows and columns are zero indexed.
 row = 0
 column = 0
-
-#Search for the posts
+print()
+# Search for the posts
 for post in get_posts(str(targetPage), pages=noOfPages):
     # write operation perform 
     try: 
-        if (len(post['text'])): #Check length of the post
+        if (len(post['text'])): # Check length of the post
             print("Scraping post #"+str(row)+": ")
             try:
                 print(post['text'])
@@ -40,8 +40,8 @@ for post in get_posts(str(targetPage), pages=noOfPages):
             print("")
     except:
         print("-------Post has no message. Skipping to the next post--------")
-#Excel saved message
+# Excel saved message
 workbook.close()
-#Debug message
-print("Successfully saved as scrappingData_"+str(targetPage)+"_FULL.xlsx")
+# Debug message
+print("Successfully saved as scrapingData_"+str(targetPage)+"_FULL.xlsx")
 print("Total rows: "+str(row-1))#Total row message
